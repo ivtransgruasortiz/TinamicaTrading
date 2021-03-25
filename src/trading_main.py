64f60ff97e0bd989a7133245af7eeb025fc956a6
ivan_tinamica <ivan.ortiz@tinamica.com>
2021-03-25 20:47:45 +0100

    Añadida variable objetivo a 5 dias

--- src/trading_main.py
+++ src/trading_main.py
@@ -39,23 +39,36 @@ def main_get_trades(escritura = True, sp500 = False):
         porcentaje_largo = alfa / (periodo_largo + 1)
         # for item in df_companies['ticker'].tolist()):
         for item in tqdm.tqdm(lista_companies):
-            data = quandl.get('WIKI/' + item, start_date="2012-09-01", end_date="2015-12-31").reset_index()
+            data = quandl.get('WIKI/' + item, start_date="2012-09-01", end_date="2016-03-31").reset_index()
             data['ticket'] = item
-            data = data[['ticket', 'Date', 'Close', 'Volume']]
-            data.columns = ['ticket', 'date', 'close', 'volume']
+            data = data[['ticket', 'Date', 'Open', 'Close', 'Volume']]
+            data.columns = ['ticket', 'date', 'open', 'close', 'volume']
             data = data.sort_values('date', ascending=True)
-            # data[['close_5', 'volume_5']] = data[['close', 'volume']].shift(periods=5, fill_value=0)
-            # data[['close_60', 'volume_60']] = data[['close', 'volume']].shift(periods=60, fill_value=0)
-            data[f'close_{periodo_corto}'] = data['close'].shift(periods=periodo_corto, fill_value=0)
-            data[f'close_{periodo_largo}'] = data['close'].shift(periods=periodo_largo, fill_value=0)
-            data[f'media_{periodo_corto}'] = round(data['close'] * porcentaje_corto + data[f'close_{periodo_corto}'] *
+            data[f'close_forward_{periodo_corto}'] = data['close'].shift(periods=-periodo_corto, fill_value=0)
+            data[f'close_backward_{periodo_corto}'] = data['close'].shift(periods=periodo_corto, fill_value=0)
+            data[f'close_backward_{periodo_largo}'] = data['close'].shift(periods=periodo_largo, fill_value=0)
+            data[f'media_{periodo_corto}'] = round(data['close'] * porcentaje_corto +
+                                                   data[f'close_backward_{periodo_corto}'] *
                                                    (1 - porcentaje_corto), 2)
-            data[f'media_{periodo_largo}'] = round(data['close'] * porcentaje_largo + data[f'close_{periodo_largo}'] *
+            data[f'media_{periodo_largo}'] = round(data['close'] * porcentaje_largo +
+                                                   data[f'close_backward_{periodo_largo}'] *
                                                    (1 - porcentaje_largo), 2)
             data['macd'] = data[f'media_{periodo_corto}'] - data[f'media_{periodo_largo}']
             data = data[(data['date'] >= '2013-01-01') & (data['date'] <= '2015-12-31')]
-            data[f'var_{periodo_corto}'] = round((data['close'] / data[f'close_{periodo_corto}']) - 1, 4) * 100
-            data[f'var_{periodo_largo}'] = round((data['close'] / data[f'close_{periodo_largo}']) - 1, 4) * 100
+            data[f'var_obj_forward_{periodo_corto}_porcentaje'] = round((data[f'close_forward_{periodo_corto}'] /
+                                                                         data['close'])
+                                                                        - 1, 4) * 100
+            data[f'var_backward_{periodo_corto}_porcentaje'] = round((data['close'] /
+                                                                      data[f'close_backward_{periodo_corto}'])
+                                                                     - 1, 4) * 100
+            data[f'var_backward_{periodo_largo}_porcentaje'] = round((data['close'] /
+                                                                      data[f'close_backward_{periodo_largo}'])
+                                                                     - 1, 4) * 100
+            data = data[['ticket', 'date', 'open', 'close', 'volume', f'close_forward_{periodo_corto}',
+                             f'close_backward_{periodo_corto}', f'close_backward_{periodo_largo}',
+                             f'media_{periodo_corto}', f'media_{periodo_largo}', 'macd',
+                             f'var_backward_{periodo_corto}_porcentaje', f'var_backward_{periodo_largo}_porcentaje',
+                             f'var_obj_forward_{periodo_corto}_porcentaje']]
             df_tot = df_tot.append(data)
         if sp500:
             df_tot.to_csv(str(root) + '/src/data/df_total_base_sp500.csv', index=False)
