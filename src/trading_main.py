38022147ee085e39864818f19b0317da4ec5fa27
ivan_tinamica <ivan.ortiz@tinamica.com>
2021-03-27 01:27:59 +0100

    v1.1

--- src/trading_main.py
+++ src/trading_main.py
@@ -11,6 +11,11 @@ import sys
 import requests as rq
 import tqdm
 import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import statsmodels
+from statsmodels.tsa.stattools import adfuller
 
 from pathlib import Path, PurePath, PureWindowsPath
 if '__file__' in locals():
@@ -72,6 +77,16 @@ def main_get_trades(escritura = True, sp500 = False):
                              f'var_obj_forward_{periodo_corto}_porcentaje']]
             df_tot = df_tot.append(data)
         if sp500:
+            df_companies_sp500_fi = pd.read_csv(str(root) + '/src/data/constituents-financials_csv.csv')
+            cols = [re.sub(' |/', '_', x.lower()) for x in df_companies_sp500_fi.columns]
+            df_companies_sp500_fi.columns = cols
+            df_companies_sp500_fi = df_companies_sp500_fi[['symbol', 'name', 'sector', 'price_earnings',
+                                                           'dividend_yield', 'market_cap', 'ebitda',
+                                                           'price_sales']]
+            df_companies_sp500_fi = df_companies_sp500_fi.rename(columns={'symbol': 'ticket'}).set_index('ticket')
+            df_tot = df_tot.set_index('ticket').join(df_companies_sp500_fi, how='left', on='ticket',
+                                                       lsuffix='_left', rsuffix='_right')
+            df_tot = df_tot.reset_index().set_index('date')
             df_tot.to_csv(str(root) + '/src/data/df_total_base_sp500.csv', index=False)
         else:
             df_tot.to_csv(str(root) + '/src/data/df_total_base.csv', index=False)
@@ -89,3 +104,69 @@ if __name__ == '__main__':
     escritura = True
     sp500 = True
     df_tot = main_get_trades(escritura, sp500)
+    df_tot_agrup_ticket = df_tot.groupby('ticket').apply(lambda x: x.sort_values(["date"], ascending=True)).reset_index(
+        drop=True)
+    df_tot_agrup_date = df_tot.groupby('date').apply(lambda x: x.sort_values(["date", 'ticket'], ascending=True)).reset_index(
+        drop=True)
+
+    ### EDA - PYTHON ###
+    df_tot_agrup_date.corr()
+    correlation_mat = df_tot_agrup_date.corr(method='spearman')
+    corr_pairs = correlation_mat.unstack()
+
+    # plot the correlation matrix of salary, balance and age in data dataframe.
+    sns.heatmap(df_tot_agrup_date[['var_obj_forward_5_porcentaje', 'macd', 'volume', 'bin_aumento_intradia']]
+                .corr(), annot=True, cmap='Reds')
+    plt.show()
+
+    # # plot the pair plot of salary, balance and age in data dataframe.
+    # sns.pairplot(data=df_tot_agrup_date, vars=['var_obj_forward_5_porcentaje', 'macd', 'volume',
+    #                                            'bin_aumento_intradia', 'media_5', 'media_60', 'close',
+    #                                            'var_backward_5_porcentaje'])
+    # plt.show()
+
+    # plot the correlation matrix of salary, balance and age in data dataframe.
+    sns.heatmap(df_tot_agrup_date[['var_obj_forward_5_porcentaje', 'macd', 'volume', 'bin_aumento_intradia',
+                                   'var_backward_5_porcentaje', 'var_backward_60_porcentaje']]
+                .corr(), annot=True, cmap='Reds')
+    plt.show()
+
+    df_tot_agrup_date.hist()
+
+    ## Histogram
+    ax = df_tot_agrup_date.hist(column='var_backward_60_porcentaje', bins=250, grid=False, figsize=(12, 8), color='#86bf91', zorder=2,
+                 rwidth=0.9)
+
+
+    ## Para la estacionariedad
+    # class StationarityTests:
+    #     def __init__(self, significance=.05):
+    #         self.SignificanceLevel = significance
+    #         self.pValue = None
+    #         self.isStationary = None
+    #
+    #
+    # def ADF_Stationarity_Test(self, timeseries, printResults=True):
+    #     # Dickey-Fuller test:
+    #     adfTest = adfuller(timeseries, autolag='AIC')
+    #
+    #     self.pValue = adfTest[1]
+    #
+    #     if (self.pValue < self.SignificanceLevel):
+    #         self.isStationary = True
+    #     else:
+    #         self.isStationary = False
+    #
+    #     if printResults:
+    #         dfResults = pd.Series(adfTest[0:4],
+    #                               index=['ADF Test Statistic', 'P-Value', '# Lags Used', '# Observations Used'])
+    #         # Add Critical Values
+    #         for key, value in adfTest[4].items():
+    #             dfResults['Critical Value (%s)' % key] = value
+    #         print('Augmented Dickey-Fuller Test Results:')
+    #         print(dfResults)
+    #
+    #     sTest = StationarityTests()
+    #     sTest.ADF_Stationarity_Test(non_stationary_series, printResults=True)
+    #     print("Is the time series stationary? {0}".format(sTest.isStationary))
+
